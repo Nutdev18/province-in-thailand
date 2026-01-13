11a6035b5e8cd403a06dcf54d88d2e200e79930a
nutdev18 <natthawat4404@gmail.com>
2026-01-13 11:02:11 +0700

    over-3

---
+++ app.js
@@ -0,0 +1,16 @@
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
