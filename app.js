46ddda118e6f811441f5df2d713923b2007d0cad
Nut-Develope <natthawat4404@gmail.com>
2026-01-14 12:59:05 +0700

    Add author names to console logs

--- app.js
+++ app.js
@@ -17,3 +17,8 @@ revealOnScroll();
 
 console.log("เว็บไซด์นี้สร้างขึ้นเพื่อส่งอาจารย์ในวิชา Web Application Development เท่านั้น");
 console.log("ขออนญาติใช้ภาพจาก Google เพื่อการศึกษา ไม่ได้มีเจตนาในเชิงพาณิชย์ใดๆ");
+console.log("จัดทำโดย")
+console.log("นาย นันทกร  พุฒพรหม")
+console.log("นาย สินธร  ยืนยิ่ง")
+console.log("นาย ณัฐวัฒน์  คนขำ")
+console.log("นาย อุดมศักดิ์  การะภักดี")
